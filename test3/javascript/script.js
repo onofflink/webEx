8c5fa22121d67a41ebe3484e28d31e17668991ec
aiegoo <eozz21@gmail.com>
2019-05-05 13:42:18 +0900

    Wrong Wrong complete overhaul of image scripts

--- test3/javascript/script.js
+++ test3/javascript/script.js
@@ -11,15 +11,28 @@ $(document).ready(function () {
     $(this).children(".submenu").stop().slideUp();
    });
 
-   start();
+/*    start();
 
-   function start(){
-     $(".imgs>img").eq(0).siblings().css({"margin-left":"-2000px"});
-     setInterval(function(){slide();},2000);
-     }
-     function slide(){
+    function start(){
+       $(".imgs>img").eq(0).siblings().css({"margin-left":"-2000px"});
+       setInterval(function(){slide();},2000);
+    }
+    function slide(){
        now = now==imgs?0:now+=1;
-       $(".imgs>img").eq(now-1).css({"margin-left":"-2000px"});
+       $(".imgs>img").eq(now-1).css({"margin-left":"-2000px"});   
        $(".imgs>img").eq(now).css({"margin-left":"0px"});
-     }
+    } */
+
+    $(".btn_slides_next").click(function(){
+      if(!$("#slides li").last().is(":visible")){
+        $(#slides li:visible).hide().next("li").fadeIn("40");
+        $(".btn_slides_prev").removeClass("off");
+      }
+      if(!$("#slides li").last().is(":visible")){
+        $(#slides li:visible).hide().next("li").fadeIn("40");
+        $(".btn_slides_next").addClass("off");
+      }
+      return false;
+    });
+ 
     })
