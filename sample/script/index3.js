20d4e151c71bc1ca6c9e15729c8acdad5347129d
aiegoo <eozz21@gmail.com>
2019-05-11 16:41:17 +0900

    complete index3.html with css and banner modification. CHECK IT OUT

--- sample/script/index3.js
+++ sample/script/index3.js
@@ -1,7 +1,19 @@
 
+var n = 0;
+var depth = 2;
+
 $(".nav").hover(function(){
 	$("ul", $(this)).stop().slideDown();
 }, function(){
 	$("ul", $(this)).stop().slideUp();
 });
 
+
+(function ani() {
+	$(".banner li").eq(n).css("z-index", depth++).fadeOut(0);
+	$(".banner li").eq(n).delay(1000).fadeIn(500, function(){
+		if(n == 4) n = 0;
+		else n++;
+		ani();
+	});
+})();
