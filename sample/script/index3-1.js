2329866b8943681c1ee43b5a4eacfae2f3933de0
onofflink <onofftony@gmail.com>
2019-05-13 08:55:46 +0900

    try out js variation of replaceWith for index3-1.html

--- sample/script/index3-1.js
+++ sample/script/index3-1.js
@@ -4,8 +4,15 @@ $(".nav").hover(function(){
   $("ul", $(this)).stop().slideUp();
 });
 
+/* $(".nav").hover(function(){
+  $("ul", $(this)).stop().slideDown();
+  $("ul > div > span", $(this)).empty().replaceWith("`<span>-</span>`");
+}, function(){
+  $("ul", $(this)).stop().slideUp();
+});
+ */
 /* $(".nav > div").click(function(){
-  $(".nav > ul").stop().css({"display":"block"});
+  $(".nav > ul").stop().empty().replaceWith();
 }, function(){
 
 }); */
