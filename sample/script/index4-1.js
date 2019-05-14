bf131f5d35070237928df5afeb9df208346d09e7
onofflink <onofftony@gmail.com>
2019-05-14 09:25:30 +0900

    conflict resolution 2

--- sample/script/index4-1.js
+++ sample/script/index4-1.js
@@ -1,11 +1,5 @@
 $(".nav").hover(function(){
-<<<<<<< HEAD
       $("ul", $(this)).stop().slideDown();
 }, function(){
       $("ul", $(this)).stop().slideUp();
-=======
-  $("ul", $(this)).stop().slideDown();
-}, function(){
-  $("ul", $(this)).stop().slideUp();
->>>>>>> 50f5bf3858125b12c9a41fc628e18f31e585f1b1
 })
