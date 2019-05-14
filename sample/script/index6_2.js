5a9e9cba36c1d557d4364f83ef74748315f53663
onofflink <onofftony@gmail.com>
2019-05-14 09:25:30 +0900

    conflict resolution 2

--- sample/script/index6_2.js
+++ sample/script/index6_2.js
@@ -9,7 +9,11 @@ $(".nav").hover(function(){
 var depth = 100;
 var n = 0;
 function ani(){
-  $(".banner li").fadeout(0);
-  $(".banner li").fadIn(0);
+  $(".banner li").eq(n).fadeOut(0).css({"z-index":depth++});
+  $(".banner li").eq(n).delay(1000).fadeIn(500, function(){
+    n++;
+    if(n == 5) n = 0;
+    ani();
+  });
 }
 ani();
