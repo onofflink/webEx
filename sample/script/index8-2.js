bc214363e142d823486e1591f3d940b9168f3e1d
onofflink <onofftony@gmail.com>
2019-05-17 11:06:10 +0900

    complete index8-2 with js html; .bts css has to be two lines

--- sample/script/index8-2.js
+++ sample/script/index8-2.js
@@ -1,4 +1,6 @@
 $(".bts").click(function(){
       var n = $(this).index();
+      $(".bts").css({"color":"#fff"});
+      $(".bts").eq(n).css({"color":"#f30"});
       $(".banner > ul").stop().animate({"left":(-n*1000)+"px"}, 500);
 })
