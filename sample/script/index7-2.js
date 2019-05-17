8f0b978833a13f1415e74ce5692961d91475eb5a
aiegoo <eozz21@gmail.com>
2019-05-17 09:04:35 +0900

    complete index7-2 with js html but button not working

---
+++ sample/script/index7-2.js
@@ -0,0 +1,24 @@
+var n = 0;
+
+$(".nav").hover(function() {
+  $("ul", $(this)).stop().slideDown();
+}, function() {
+  $("ul", $(this)).stop().slideUp();
+});
+
+var n = 0;
+
+$("#bt_next").click(function(){
+	if(n < 4) n++;
+	ani();
+});
+
+$("#bt_prev").click(function(){
+	if(n > 0) n--;
+	ani();
+});
+
+function ani() {
+	$(".banner > ul").stop().animate({"left":(-n*1000)+"px"}, 500);
+}
+
