a46acb0f3f96fa2a2d0c48b3ae350e6fc33a26a9
aiegoo <eozz21@gmail.com>
2019-05-20 04:28:16 +0900

    complete index5-3 with js css and tags; emmet changed, banner not working

--- sample/script/index5-3.js
+++ sample/script/index5-3.js
@@ -1,6 +1,18 @@
+//jshint jsversion:6
 var n = 0;
+$(".nav").hover(function(){
+  $("ul", $(this)).stop().slideDown();
+}, function(){
+  $("ul", $(this)).stop().slideUp();
+})
+
 (function ani(){
-  $(".banner").animate({"left":"-=n*1000"+"px"}, 500, function(){
-    
-  })
+  $(".banner > ul").delay(2000).animate({"left":"-=n*1000"+"px"}, 500, function(){
+    n++;
+    if(n == 5) {
+      n = 0;
+      $(this).css({"left":"0px"});
+    }
+  ani();
+  });
 })();
