8fa9d3db37e6b176b41790a42f75e9378e26dc4a
aiegoo <eozz21@gmail.com>
2019-05-20 08:42:22 +0900

    fixt index5-2 tags by moving section to above banner and rm contents div; -=n* to -=1000px

--- sample/script/index5-3.js
+++ sample/script/index5-3.js
@@ -4,10 +4,10 @@ $(".nav").hover(function(){
   $("ul", $(this)).stop().slideDown();
 }, function(){
   $("ul", $(this)).stop().slideUp();
-})
+});
 
 (function ani(){
-  $(".banner > ul").delay(2000).animate({"left":"-=n*1000"+"px"}, 500, function(){
+  $(".banner > ul").delay(2000).animate({"left":"-=1000px"}, 500, function(){
     n++;
     if(n == 5) {
       n = 0;
