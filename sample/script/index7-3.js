0dc3bfe634774cdca6b996d468f5d272c9cb43f6
aiegoo <eozz21@gmail.com>
2019-05-20 23:56:09 +0900

    complete index7-3; fixed up tags and typo in js btn bt_next

--- sample/script/index7-3.js
+++ sample/script/index7-3.js
@@ -6,12 +6,12 @@ $(".nav").hover(function(){
 
 var n = 0;
 
-$("#bt_next").click(function(){
+$("#btn_next").click(function(){
 	if(n < 4) n++;
 	ani();
 });
 
-$("#bt_prev").click(function(){
+$("#btn_prev").click(function(){
 	if(n > 0) n--;
 	ani();
 });
