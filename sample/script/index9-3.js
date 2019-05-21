8280471620175438a86d73abb21bfcd20feaf50b
onofflink <onofftony@gmail.com>
2019-05-21 11:58:30 +0900

    fix modal but not yet with popup for index9-3

--- sample/script/index9-3.js
+++ sample/script/index9-3.js
@@ -1,13 +1,13 @@
 (function winOpen(){
-      window.open("win.html", "popup", "width=400, height=400, top=200, left=200");
+      window.open("win.html", "win", "width=400, height=400, top=200, left=200");
 });
 
-function modalOpen () {
+function modalOpen() {
       $("#modal_bg").show(0);
-      $("#modal_bg").css({"display": "table"});
+      $("#modal_wrap").css({"display": "table"});
 }
 
-function modalClose () {
+function modalClose() {
       $("#modal_bg").hide(0);
-      $("#modal_bg").css({"display": "none"});
+      $("#modal_wrap").css({"display": "none"});
 }
