18442599dd55e2916d2d8f771b6cf56b73b5bb2e
aiegoo <eozz21@gmail.com>
2019-05-22 09:18:13 +0900

    fix up banner, but not with navs; z-index?

---
+++ exam0/script/index.js
@@ -0,0 +1,17 @@
+$(".nav").hover(function(){
+  $("ul", $(this)).stop().slideDown();
+}, function() {
+  $("ul", $(this)).stop().slideUp();
+});
+
+var n = 0;
+(function ani() {
+  $(".banner > ul").delay(2000).animate({"left":"-=1000px"}, 5000, function() {
+    /* if(n = n==ul?0:n+=1)  */n++;
+    if(n == 5) {
+      n = 0;
+      $(this).css({"left": "0px"});
+      }
+            ani();
+      });
+})();
