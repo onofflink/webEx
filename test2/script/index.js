ae534031e096f30fdce7cb59c854a4138a170bc0
aiegoo <eozz21@gmail.com>
2019-05-25 17:46:37 +0900

    complete test2 crash tryout with css and js; more to come

--- test2/script/index.js
+++ test2/script/index.js
@@ -1,16 +1,17 @@
 $(".nav").hover(function(){
   $("ul", $(this)).stop().slideDown();
-}, function() {
+}, function(){
   $("ul", $(this)).stop().slideUp();
 });
 
 var n = 0;
 (function ani(){
-  $(".banner > ul > li").delay(2000).animate({"margin-left":"-=960px"}, 2000, function(){
+  $(".slide").delay(2000).animate({"left":"-=960px"}, 2000, function(){
     n++;
-    if(n = 5) {
-     $(".banner > ul > li").css("margin-left", "0px");
+    if(n == 5) {
+      n = 0;
+      $(this).css({"left":"0px"});
     }
     ani();
-  });  
+  });
 })();
