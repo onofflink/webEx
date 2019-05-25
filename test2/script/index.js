250c86b72107fea36404feb68a866972f5295c65
aiegoo <eozz21@gmail.com>
2019-05-25 14:42:02 +0900

    add css and js for banners to test2 index

--- test2/script/index.js
+++ test2/script/index.js
@@ -3,3 +3,14 @@ $(".nav").hover(function(){
 }, function() {
   $("ul", $(this)).stop().slideUp();
 });
+
+var n = 0;
+(function ani(){
+  $(".banner > ul > li").delay(2000).animate({"margin-left":"-=960px"}, 2000, function(){
+    n++;
+    if(n = 5) {
+     $(".banner > ul > li").css("margin-left", "0px");
+    }
+    ani();
+  });  
+})();
