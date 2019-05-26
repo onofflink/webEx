f77fc27c8e00b9f443ff06bbd46abb8bf0cfd9be
aiegoo <eozz21@gmail.com>
2019-05-26 22:25:30 +0900

    add css and js to test4 index to diff with test3-tmp

--- test4/scripts/index.js
+++ test4/scripts/index.js
@@ -2,4 +2,28 @@ $(".nav").hover(function(){
   $("ul", $(this)).stop().slideDown();
 }, function(){
   $("ul", $(this)).stop().slideUp();
-})
+});
+
+/* var n = 0;
+(function ani(){
+  $(".banner > ul").delay(2000).animate({"left": "-=1000px"}, 2000, function(){
+    n++;
+    if(n == 3){
+      n = 0;
+      $(this).css("left","0px");
+    }
+    ani();
+  })
+})(); */
+
+var n = 0;
+var depth = 100;
+function ani(){
+  $(".banner li").eq(n).fadeOut(0).css({"z-index": depth++});
+  $(".banner li").eq(n).delay(2000).fadeIn(2000, function(){
+    n++;
+    if(n == 3) n = 0;
+    ani();
+  });
+}
+ani();
