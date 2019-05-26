e05f31edfb4e29526d59f0276318e46825417910
aiegoo <eozz21@gmail.com>
2019-05-26 12:16:36 +0900

    add banner and navs to test3 index css and js, but not working

--- test3/script/index.js
+++ test3/script/index.js
@@ -3,3 +3,22 @@ $(".nav").hover(function(){
 }, function(){
      $("ul", $(this)).stop().slideUp();
 });
+
+var n = 0;
+$(".btn_prev").click(function(){
+     if(n > 0) {
+          n--;
+          ani();
+     }
+});
+
+$(".btn_next").click(function(){
+     if(n < 3) {
+          n++;
+          ani();
+     }
+});
+
+function ani() {
+     $(".banner > ul").animate({"left": "-n*1000px"}, 2000);
+}
