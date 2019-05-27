8799d68e797c1027e2ee5295d9e747951ce07cef
aiegoo <eozz21@gmail.com>
2019-05-28 08:46:51 +0900

    modal difftool with b70cba3

--- sample/script/index9-modal.js
+++ sample/script/index9-modal.js
@@ -11,3 +11,13 @@ function modalClose(){
      $("#modal-bg").hide(0);
      $("#modal-wrap").css({"display": "none"});
 }
+
+var n = 0;
+(function ani(){
+     $(".slide").delay(2000).animate({"left": "-=200px"}, 3000, function(){
+          n++;
+          if(n == 2) n = 0;
+          $(this).animate({"left": "0px"}, 2000);
+          ani();
+     });
+})();
