66924e0e227c278c3f8ad10f5a79cac88a4466c7
aiegoo <eozz21@gmail.com>
2019-06-02 05:15:25 +0900

    fix index 3 script css and tags

--- sample/script/index3.js
+++ sample/script/index3.js
@@ -1,19 +1,21 @@
 
 var n = 0;
 var depth = 2;
-
 $(".nav").hover(function(){
 	$("ul", $(this)).stop().slideDown();
-}, function(){
+   }, function(){
 	$("ul", $(this)).stop().slideUp();
-});
+   });
 
 
-(function ani() {
-	$(".banner li").eq(n).css("z-index", depth++).fadeOut(0);
-	$(".banner li").eq(n).delay(1000).fadeIn(500, function(){
-		if(n == 4) n = 0;
-		else n++;
-		ani();
-	});
+var n = 0;
+(function ani(){
+  $(".banner > ul").delay(2000).animate({"left":"-=1000px"}, 2000, function(){
+    n++;
+    if(n == 5){
+      n = 0;
+      $(this).animate({"left": "0px"}, 2000);
+    }
+    ani();
+  });
 })();
