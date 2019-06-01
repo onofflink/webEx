1f287acd110c5999a34954685a7fff705b26fd10
aiegoo <eozz21@gmail.com>
2019-06-01 19:36:41 +0900

    finally done with banner-btn slides, mv z-index to .slide from banner

--- exam5/script/index.js
+++ exam5/script/index.js
@@ -4,18 +4,13 @@ $(".nav").hover(function(){
      $(".submenu", $(this)).stop().slideUp();
 });
 
+
 $(".bts").click(function(){
-	var n = $(this).index();
-	$(".bts").css({"color":"#fff"});
-	$(".bts").eq(n).css({"color":"#f30"});
-	$(".banner > ul").stop().animate({"left":(-n*1000)+"px"}, 500);
-});
-/* $(".bts").click(function(){
      var n = $(this).index();
-     $(".bts").css({"color": "#fff"});
-     $(".bts").eq(n).css({"color": "#f30"});
-     $(".slide").stop().animate({"left": (-n*1000)+"px"}, 5000);
-}); */
+     $(".bts").css({"color":"#fff"});
+     $(".bts").eq(n).css({"color":"#f30"});
+     $(".slide").stop().animate({"left":(-n*1000)+"px"}, 500);
+});
 
 function popUp() {
      window.open("contact.html");
