e76f6cb3d5f0b738239e99a6e4203ff95784e429
aiegoo <eozz21@gmail.com>
2019-06-03 01:02:32 +0900

    add gallery fade animation and trying on partner bts animation on exam6 actual test on june2

--- exam6/script/index.js
+++ exam6/script/index.js
@@ -30,3 +30,21 @@ function modalClose(){
 function popUp(){
       window.open("popup.html", "greenHouse", "width=500, height=200, top=200, left=300, toolbar=no, scrollbars=no, menubar=no, location=no, status=no, resizable=no")
 }
+
+var i = 0;
+var depth = 20;
+(function gallery(){
+      $(".gallery-wrap li").eq(i).css("z-index", depth++).fadeOut(0);
+      $(".gallery-wrap li").eq(i).delay(2000).fadeIn(2000, function(){
+            if(i == 2) i = 0;
+            else i++;
+            gallery();
+      });
+})();
+
+$(".bts").click(function(){
+      var x = $(this).index();
+      $(".bts").css("color", "#f09");
+      $(".bts").eq(x).css("color", "#f30");
+      $(".partner_info").eq(x).animate({"left": (-x*100)+"px"}, 2000);
+});
