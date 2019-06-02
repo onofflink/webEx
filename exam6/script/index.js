f11d8a01c2aaee866533c33877c1c89c343f5bc2
aiegoo <eozz21@gmail.com>
2019-06-02 22:21:30 +0900

    add popup modal and finetune details for exam6 of actual test june 2

--- exam6/script/index.js
+++ exam6/script/index.js
@@ -5,14 +5,28 @@ $(".navs").hover(function(){
       $(".submenu", $(this)).stop().slideUp();
 });
 
-var n = 0;
+var n = 0; 
 (function ani(){
-      $(".slide").delay(3000).animate({"left": "-=1200px"}, 3000, function(){
+      $(".slide").delay(1000).animate({"left": "-=1200px"}, 3000, function(){
             n++;
-            if(n == 4) {
-                  n = 0;
-                  $(this).animate({"left": "0px"}, 3000);
+            if(n == 4) { 
+                  n = 0;                 
+                  $(this).animate({"left": "0px"}, 12000);
             }
-            ani();
+            ani(); 
       });      
 })();
+
+function modalOpen(){
+      $("#modal-bg").show(0);
+      $("#modal-wrap").css("display", "table");
+}
+
+function modalClose(){
+      $("#modal-bg").hide(0);
+      $("#modal-wrap").css("display", "none");
+}
+
+function popUp(){
+      window.open("popup.html", "greenHouse", "width=500, height=200, top=200, left=300, toolbar=no, scrollbars=no, menubar=no, location=no, status=no, resizable=no")
+}
