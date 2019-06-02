1bd858d3d50dda8de4b73e01be62681e67c669e3
onofflink <onofftony@gmail.com>
2019-06-02 17:14:06 +0900

    add except modal and popup and overall detals for the page

--- exam6/script/index.js
+++ exam6/script/index.js
@@ -4,3 +4,15 @@ $(".navs").hover(function(){
 }, function() {
       $(".submenu", $(this)).stop().slideUp();
 });
+
+var n = 0;
+(function ani(){
+      $(".slide").delay(3000).animate({"left": "-=1200px"}, 3000, function(){
+            n++;
+            if(n == 4) {
+                  n = 0;
+                  $(this).animate({"left": "0px"}, 3000);
+            }
+            ani();
+      });      
+})();
