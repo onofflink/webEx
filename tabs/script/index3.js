220b1805f65bcd5feddc3fc8b4f9d163a352f522
aiegoo <eozz21@gmail.com>
2019-06-11 22:08:49 +0900

    add tab3 with replace and fadeIn method

---
+++ tabs/script/index3.js
@@ -0,0 +1,10 @@
+(function($){
+     var tabs = $(".tabs li a");
+     tabs.click(function(){
+          var content = this.hash.replace("/", "");
+          tabs.removeClass("active");
+          $(this).addClass("active");
+          $("#content").find('p').hide();
+          $(content).fadeIn(300);
+     });
+})(jQuery);
