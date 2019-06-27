1bf273f99c478ac744ed5a0a56d95790f89c5dd8
aiegoo <eozz21@gmail.com>
2019-06-28 04:55:06 +0900

    add tab/script index3.js as a practice using replace method

--- tabs/script/index3.js
+++ tabs/script/index3.js
@@ -1,29 +1,10 @@
 (function(){
-     var tabs = $(".tabs li a");
-     tabs.click(function(){
-          var content = this.hash.replace("/", "");
-          tabs.removeClass("active");
-          $(this).addClass("active");
-          $("#content").find('p').hide();
-          $(content).fadeIn(300);
-     });
-})();
-
-/* 
-$('.tab-content>div').hide();
-$('.tab-content>div').first().slideDown();
-  $('.tab-buttons span').click(function(){
-    var thisclass=$(this).attr('class');
-    $('#lamp').removeClass().addClass('#lamp').addClass(thisclass);
-    $('.tab-content>div').each(function(){
-      if($(this).hasClass(thisclass)){
-        $(this).fadeIn(800);
-      }
-      else{
-        $(this).hide();
-      }
-    });
+  var tabs = $(".tabs li a");
+  tabs.click(function(){
+    var abcd = this.hash.replace("/", "");
+    tabs.removeClass("active");
+    $(this).addClass("active");
+    $("#content").find('p').hide();
+    $(abcd).fadeIn(500);
   });
-
-
-*/
+})();
