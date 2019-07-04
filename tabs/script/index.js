fd412da5dc9943f33970584df5f4bb563ea8f25c
aiegoo <eozz21@gmail.com>
2019-07-04 09:02:05 +0900

    error in reference to .tab__content; use this instead index.js of tabs

--- tabs/script/index.js
+++ tabs/script/index.js
@@ -3,7 +3,8 @@
      let height = -1;
      $(".tab__content").each(function(){
           height = height > $(this).outerHeight() ? height: $(this).outerHeight();
-          $(this).css("position", "absolute");          
+
+          $(this).css("position", "absolute");
      });
      $("[data-tabs]").css("min-height", height+40+"px");
-}(jQuery, document));
+})(jQuery, document);
