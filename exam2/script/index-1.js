583884102d3374dfda5a5904f1c25dbb203c06e2
aiegoo <eozz21@gmail.com>
2019-07-10 00:08:25 +0900

    add index1.js to exam1 and index-1.js to exam2 of review

--- exam2/script/index-1.js
+++ exam2/script/index-1.js
@@ -6,11 +6,11 @@ $(".nav").hover(function(){
 
 var n = 0;
 (function ani(){
-     $(".slide").delay(2000).animate({"left": "-=960px"}, 2000, function(){
+     $(".slide").delay(2000).animate({"left":"-=960px"}, 2000, function(){
           n++;
-          if (n==5) {
+          if(n == 4){
                n = 0;
-               $(this).css("left", "0px");
+               $(this).animate({"left": "0px"}, 2000);
           }
           ani();
      });
@@ -21,12 +21,11 @@ function modalOpen() {
      $("#modal-wrap").css("display", "table-cell");
 }
 
-
 function modalClose() {
      $("#modal-bg").hide(0);
      $("#modal-wrap").css("display", "none");
 }
 
-function popUp() {
-     window.open("contact.html", "win", "width=500, height=500, top=50, left=50, scrollbars=no, menubar=no, toolbar=no, status=no, location=no, resizable=no")
+function popUp(){
+     window.open("contact.html", "win", "width=800, height=500, top=200, left=300, toolbar = no, location= no , status = no, menubar = no, resizable = no , scrollbars = no")
 }
