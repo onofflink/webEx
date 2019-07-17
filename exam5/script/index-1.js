91bdcdd321bbcdf7a2adaf2e4180be57b6c82030
aiegoo <eozz21@gmail.com>
2019-07-18 07:42:55 +0900

    add index1-1 to exam5 of review

---
+++ exam5/script/index-1.js
@@ -0,0 +1,18 @@
+$(".nav").hover(function(){
+     $(".submenu", $(this)).stop().slideDown();
+}, function(){
+     $(".submenu", $(this)).stop().slideUp();
+});
+
+
+
+$(".bts").click(function(){
+     var n = $(this).index();
+     $(".bts").css({"color":"#fff"});
+     $(".bts").eq(n).css({"color":"#f30"});
+     $(".slide").stop().animate({"left":(-n*1000)+"px"}, 500);
+});
+
+function popUp() {
+     window.open("contact.html");
+}
--- exam5/script/index.js
+++ exam5/script/index.js
@@ -1,18 +1,17 @@
 $(".nav").hover(function(){
-     $(".submenu", $(this)).stop().slideDown();
+     $("ul", $(this)).stop().slideDown();
 }, function(){
-     $(".submenu", $(this)).stop().slideUp();
+     $("ul", $(this)).stop().slideUp();
 });
-
-
 
 $(".bts").click(function(){
      var n = $(this).index();
-     $(".bts").css({"color":"#fff"});
-     $(".bts").eq(n).css({"color":"#f30"});
-     $(".slide").stop().animate({"left":(-n*1000)+"px"}, 500);
+     $(".bts").css({"color": "#fff"});
+     $(".bts").eq(n).css({"color": "#f30"});
+     $(".slide").stop().animate({"left": (-n*1000)+"px"},500);
 });
 
 function popUp() {
      window.open("contact.html");
+     
 }
