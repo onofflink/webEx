91878d3a219be6fb1400e1c24978d4593d16d136
aiegoo <eozz21@gmail.com>
2019-08-19 20:25:23 +0900

    add index9a.css and index1-1.js

--- exam5/script/index1-1.js
+++ exam5/script/index1-1.js
@@ -68,3 +68,18 @@ function ani() {
      $(".slide").eq(n).animate({"left": (-n*1000)+"px"}, 2000);
 })
  */
+
+var Content = $(".content > div");
+$(Content).hide();
+$(Content).last().slideDown(500);
+$(".tab-buttons span").click(function(){
+     var Tabs = $(this).attr("class");
+     $("#lamp").removeClass().addClass("#lamp").addClass(Tabs);
+     $(Content).each(function(){
+          if($(this).hasClass(Tabs)){
+               $(this).fadeIn(800);
+          }else{
+               $(this).hide();
+          }
+     });
+});
