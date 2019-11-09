1bab38f4be35c5f88db6b177fa4b6e2128bf35a7
aiegoo <eozz21@gmail.com>
2019-11-10 07:05:30 +0900

    complete test1 1123.html

--- test1/script/1123.js
+++ test1/script/1123.js
@@ -8,6 +8,25 @@ $(".nav").hover(function(){
 var n = 0;
 (function ani(){
      $(".banner > ul").delay(2000).animate({"left": "-=1000px"}, 2000, function(){
-          
+          n++;
+          if(n==5) {
+            n=0;
+            $(this).css("left", "0px");
+          }
+          ani();
      });
 });
+
+function modalOpen() {
+  $("modal-bg").show(0);
+  $("modal-wrap").css("display", "table-cell");
+}
+
+function modalClose() {
+  $("modal-bg").hide(0);
+  $("modal-wrap").css("display", "none");
+}
+
+function winOpen() {
+  window.open("win.html", "win", "width=55, height=500, left=200, top=200, scrollbars=no, toolbar=no, menubar=no, status=no, resizable=no, location=no");
+}
