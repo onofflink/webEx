18cc47d5686439e6a2c037d3a89090f8db426b09
aiegoo <eozz21@gmail.com>
2020-08-31 23:14:24 +0900

    update index2020b css and part of js

--- exam6/script/index2020b.js
+++ exam6/script/index2020b.js
@@ -3,3 +3,17 @@ $(".navs").hover(function(){
 }, function(){
      $(".submenu", $(this)).stop().slideUp();     
 });
+
+var n = 0;
+(function ani(){
+     $(".banner ul").delay(2000).animate({
+          "left": "-=1000px"
+     }, 2000, function(){
+          n++;
+          if (n==3) {
+               n=0;
+               $(this).css("left", "0px");
+          }
+          ani();
+     });
+});
